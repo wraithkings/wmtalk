04faf29a88e7b86404233260403d70d7bf27ac05
wangxuesong <wangxuesong@thtf.com.cn>
2010-03-08 13:01:42 +0800

    init

---
+++ talk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace talk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
